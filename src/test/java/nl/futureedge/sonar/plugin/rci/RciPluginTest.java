0f3383273855f7c4ec72aca10af7b67e251f75e6
Robert Willems of Brilman <robert.willems.of.brilman@gmail.com>
2016-11-23 22:31:22 +0100

    Clean up

--- src/test/java/nl/futureedge/sonar/plugin/rci/RciPluginTest.java
+++ src/test/java/nl/futureedge/sonar/plugin/rci/RciPluginTest.java
@@ -10,9 +10,6 @@ public class RciPluginTest {
 	@Test
 	public void test() {
 		final RciPlugin javaPlugin = new RciPlugin();
-		// final SonarRuntime runtime =
-		// SonarRuntimeImpl.forSonarQube(Version.create(5, 6),
-		// SonarQubeSide.SERVER);
 		final Plugin.Context context = new Plugin.Context(SonarQubeVersion.V5_6);
 
 		Assert.assertEquals(0, context.getExtensions().size());
