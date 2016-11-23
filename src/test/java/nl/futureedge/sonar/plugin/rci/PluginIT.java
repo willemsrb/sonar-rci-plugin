951237e2fecb49c785c54aad5507376dbae4dee3
Robert Willems of Brilman <robert.willems.of.brilman@gmail.com>
2016-11-23 20:22:39 +0100

    Reverted plugin api to version 5.6; Added integration test to test compatibility with 5.6, LTS, 6.0 and 6.1

---
+++ src/test/java/nl/futureedge/sonar/plugin/rci/PluginIT.java
@@ -0,0 +1,80 @@
+package nl.futureedge.sonar.plugin.rci;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.sonar.wsclient.services.Measure;
+import org.sonar.wsclient.services.Resource;
+import org.sonar.wsclient.services.ResourceQuery;
+
+import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.build.MavenBuild;
+import com.sonar.orchestrator.locator.FileLocation;
+
+@RunWith(Parameterized.class)
+public class PluginIT {
+
+	@Parameters
+	public static Collection<Object[]> sonarQubeVersions() {
+		return Arrays.asList(new Object[][] { { "5.6" }, { "LTS" }, { "6.0" }, { "6.1" } });
+	}
+
+	private final String sonarQubeVersion;
+	private Orchestrator orchestrator;
+
+	public PluginIT(final String sonarQubeVersion) {
+		this.sonarQubeVersion = sonarQubeVersion;
+	}
+
+	@Before
+	public void setupSonarQube() {
+		System.getProperties().setProperty("sonar.runtimeVersion", sonarQubeVersion);
+
+		orchestrator = Orchestrator.builderEnv()
+				.addPlugin(FileLocation.byWildcardMavenFilename(new File("target"), "sonar-rci-plugin-*.jar"))
+				.setOrchestratorProperty("javaVersion", "4.2.1").addPlugin("java").build();
+
+		orchestrator.start();
+	}
+
+	@After
+	public void teardownSonarQube() {
+		if (orchestrator != null) {
+			orchestrator.stop();
+		}
+	}
+
+	public void runSonar() {
+		final File pom = new File(new File(".", "target/it"), "pom.xml");
+
+		final MavenBuild install = MavenBuild.create(pom).setGoals("clean verify");
+		Assert.assertTrue("'clean verify' failed", orchestrator.executeBuild(install).isSuccess());
+
+		final MavenBuild sonar = MavenBuild.create(pom).setGoals("sonar:sonar");
+		Assert.assertTrue("'sonar:sonar' failed", orchestrator.executeBuild(sonar).isSuccess());
+	}
+
+	@Test
+	public void test() {
+		runSonar();
+
+		final ResourceQuery query = ResourceQuery.createForMetrics(
+				"nl.future-edge.sonarqube.plugins:sonar-rci-plugin-it:src/main/java/TestClass.java",
+				RciMetrics.RULES_COMPLIANCE_INDEX.getKey());
+
+		final Resource resource = orchestrator.getServer().getWsClient().find(query);
+		final Measure measure = resource.getMeasure(RciMetrics.RULES_COMPLIANCE_INDEX.getKey());
+		final Double value = measure.getValue();
+
+		Assert.assertEquals(55.6d, value, 0.01);
+	}
+
+}
--- src/test/java/nl/futureedge/sonar/plugin/rci/RciPluginTest.java
+++ src/test/java/nl/futureedge/sonar/plugin/rci/RciPluginTest.java
@@ -3,18 +3,17 @@ package nl.futureedge.sonar.plugin.rci;
 import org.junit.Assert;
 import org.junit.Test;
 import org.sonar.api.Plugin;
-import org.sonar.api.SonarQubeSide;
-import org.sonar.api.SonarRuntime;
-import org.sonar.api.internal.SonarRuntimeImpl;
-import org.sonar.api.utils.Version;
+import org.sonar.api.SonarQubeVersion;
 
 public class RciPluginTest {
 
 	@Test
 	public void test() {
 		final RciPlugin javaPlugin = new RciPlugin();
-		final SonarRuntime runtime = SonarRuntimeImpl.forSonarQube(Version.create(5, 6), SonarQubeSide.SERVER);
-		final Plugin.Context context = new Plugin.Context(runtime);
+		// final SonarRuntime runtime =
+		// SonarRuntimeImpl.forSonarQube(Version.create(5, 6),
+		// SonarQubeSide.SERVER);
+		final Plugin.Context context = new Plugin.Context(SonarQubeVersion.V5_6);
 
 		Assert.assertEquals(0, context.getExtensions().size());
 		javaPlugin.define(context);
